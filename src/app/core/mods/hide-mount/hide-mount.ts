a7c81a058b97a3e9f52567b1e538fd3836fec7f1
Programmateur01 <git@juliencoutault.fr>
2019-11-08 00:22:53 +0100

    [MOD][HIDE-MOUNT] Remove timeout

--- src/app/core/mods/hide-mount/hide-mount.ts
+++ src/app/core/mods/hide-mount/hide-mount.ts
@@ -2,8 +2,8 @@ import {Mods} from "../mods";
 import {Logger} from "app/core/electron/logger.helper";
 
 /**
- * This mod add the possibility to hide mount durring fight
- */
+* This mod add the possibility to hide mount durring fight
+*/
 export class HideMount extends Mods
 {
     constructor (wGame: any | Window, private hidden_mount: boolean) {
@@ -11,53 +11,49 @@ export class HideMount extends Mods
 
         // feature active
         if (this.hidden_mount) {
-            Logger.info(' - enable Hide-Mount');
+            Logger.info('- enable Hide-Mount');
             let toggle = () => {
                 try {
-                    setTimeout(() => {
-                        let actionNeeded = false;
-                        // Main character
-                        if (this.wGame.actorManager.userActor.actorManager.userActor.look.subentities != null) {
-                            if (this.wGame.actorManager.userActor.actorManager.userActor.look.subentities.length > 0) {
+                    let actionNeeded = false;
+                    // Main character
+                    if (this.wGame.actorManager.userActor.actorManager.userActor.look.subentities != null) {
+                        if (this.wGame.actorManager.userActor.actorManager.userActor.look.subentities.length > 0) {
+                            actionNeeded = true;
+                            this.wGame.actorManager.userActor.actorManager.userActor.look.bonesId = 1;
+                            if (this.wGame.actorManager.userActor.actorManager.userActor.look.subentities["0"].subEntityLook.skins.length > 0) {
+                                this.wGame.actorManager.userActor.actorManager.userActor.look.skins = this.wGame.actorManager.userActor.actorManager.userActor.look.subentities["0"].subEntityLook.skins;
+                            }
+                            // Character size
+                            this.wGame.actorManager.userActor.actorManager.userActor.look.scales = [140];
+                            if (this.wGame.actorManager.userActor.look.subentities["0"].subEntityLook.indexedColors.length > 0) {
+                                this.wGame.actorManager.userActor.actorManager.userActor.look.indexedColors = this.wGame.actorManager.userActor.look.subentities["0"].subEntityLook.indexedColors;
+                            }
+                            this.wGame.actorManager.userActor.actorManager.userActor.look.subentities = null;
+                        }
+                    }
+                    // Other character
+                    for (let key in this.wGame.actorManager.actors) {
+                        if (+key > 0) {
+                            if (this.wGame.actorManager.actors[key].look.subentities != null) {
                                 actionNeeded = true;
-                                this.wGame.actorManager.userActor.actorManager.userActor.look.bonesId = 1;
-                                if (this.wGame.actorManager.userActor.actorManager.userActor.look.subentities["0"].subEntityLook.skins.length > 0) {
-                                    this.wGame.actorManager.userActor.actorManager.userActor.look.skins = this.wGame.actorManager.userActor.actorManager.userActor.look.subentities["0"].subEntityLook.skins;
+                                this.wGame.actorManager.actors[key].look.bonesId = 1;
+                                this.wGame.actorManager.actors[key].look.scales = [140];
+                                if (this.wGame.actorManager.actors[key].look.subentities["0"].subEntityLook.skins.length > 0) {
+                                    this.wGame.actorManager.actors[key].look.skins = this.wGame.actorManager.actors[key].look.subentities["0"].subEntityLook.skins;
                                 }
-                                // Character size
-                                this.wGame.actorManager.userActor.actorManager.userActor.look.scales = [140];
-                                if (this.wGame.actorManager.userActor.look.subentities["0"].subEntityLook.indexedColors.length > 0) {
-                                    this.wGame.actorManager.userActor.actorManager.userActor.look.indexedColors = this.wGame.actorManager.userActor.look.subentities["0"].subEntityLook.indexedColors;
+                                if (this.wGame.actorManager.actors[key].look.subentities["0"].subEntityLook.indexedColors.length > 0) {
+                                    this.wGame.actorManager.actors[key].look.indexedColors = this.wGame.actorManager.actors[key].look.subentities["0"].subEntityLook.indexedColors;
                                 }
-                                this.wGame.actorManager.userActor.actorManager.userActor.look.subentities = null;
+                                this.wGame.actorManager.actors[key].look.subentities = null;
                             }
                         }
-                        // Other character
-                        for (let key in this.wGame.actorManager.actors) {
-                            if (+key > 0) {
-                                if (this.wGame.actorManager.actors[key].look.subentities != null) {
-                                    actionNeeded = true;
-                                    this.wGame.actorManager.actors[key].look.bonesId = 1;
-                                    this.wGame.actorManager.actors[key].look.scales = [140];
-                                    if (this.wGame.actorManager.actors[key].look.subentities["0"].subEntityLook.skins.length > 0) {
-                                        this.wGame.actorManager.actors[key].look.skins = this.wGame.actorManager.actors[key].look.subentities["0"].subEntityLook.skins;
-                                    }
-                                    if (this.wGame.actorManager.actors[key].look.subentities["0"].subEntityLook.indexedColors.length > 0) {
-                                        this.wGame.actorManager.actors[key].look.indexedColors = this.wGame.actorManager.actors[key].look.subentities["0"].subEntityLook.indexedColors;
-                                    }
-                                    this.wGame.actorManager.actors[key].look.subentities = null;
-                                }
-                            }
-                        }
-                        if (actionNeeded) {
-                            setTimeout(() => {
-                                this.wGame.gui.mainControls._creatureModeButton.tap();
-                                setTimeout(() => {
-                                    this.wGame.gui.mainControls._creatureModeButton.tap();
-                                }, (Math.random() * (500) + 500));
-                            }, (Math.random() * (500) + 500));
-                        }
-                    }, (Math.random() * (500) + 500));
+                    }
+                    if (actionNeeded) {
+                        this.wGame.gui.mainControls._creatureModeButton.tap();
+                        setTimeout(() => {
+                            this.wGame.gui.mainControls._creatureModeButton.tap();
+                        }, 100);
+                    }
                 } catch (ex) {
                     Logger.info(ex);
                 }
@@ -65,9 +61,6 @@ export class HideMount extends Mods
 
             // Wait begin fight
             this.on(this.wGame.dofus.connectionManager, 'GameFightStartMessage', toggle);
-        } else {
-          Logger.info('- disable Hide-Mount');
         }
-
     }
 }
