e66ab303d4584976f4fa8f6c784f56522f59db0a
Programmateur01 <git@juliencoutault.fr>
2020-01-10 00:30:53 +0100

    [FIX] revert files
    revert

---
+++ src/app/core/mods/jobsxp/jobsxp.ts
@@ -0,0 +1,211 @@
+import { Mods } from "../mods";
+import { TranslateService } from "@ngx-translate/core";
+import { Option } from "app/core/service/settings.service";
+import { Logger } from "app/core/electron/logger.helper";
+
+export class Jobsxp extends Mods{
+    private xpRestanteText : HTMLDivElement;
+
+    constructor(
+        wGame: any,
+        private params: Option.VIP.General,
+        private translate: TranslateService
+    ) {
+        super(wGame);
+
+        if (this.params.jobsxp) {
+            Logger.info('- enable jobsxp');
+            let jobsxpbarCssverif = this.wGame.document.getElementById('jobsxpbarCss');
+            let xpRestanteIdverif = this.wGame.document.getElementById('xpRestanteId');
+            if (jobsxpbarCssverif && jobsxpbarCssverif.parentElement) {
+                jobsxpbarCssverif.parentElement.removeChild(jobsxpbarCssverif);
+            }
+            if (xpRestanteIdverif && xpRestanteIdverif.parentElement) {
+                xpRestanteIdverif.parentElement.removeChild(xpRestanteIdverif);
+            }
+
+            let jobsxpbarCss = document.createElement('style');
+            jobsxpbarCss.id = 'jobsxpbarCss';
+            jobsxpbarCss.innerHTML = `
+            .xpRestanteText {
+                box-sizing: border-box;
+                overflow: hidden;
+                font-size: 11px;
+                position: absolute;
+                color: white;
+                margin-right: 10px;
+                margin-top: 10px;
+                text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.9);
+                right: 10px;
+                pointer-events: none;
+                padding: 5px 16px;
+            }
+
+            .xpRestanteText::after {
+                content: '';
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                border-image-source: url(./assets/ui/container.png);
+                border-image-slice: 63;
+                border-image-width: 37px;
+                border-radius: 10px;
+                border-style: solid;
+                background-color: #2e2d28;
+                z-index: -1;
+                box-sizing: border-box;
+                opacity: 0.8;
+            }
+
+            .xpRestanteText .job {
+                display: flex;
+                margin: 8px;
+                align-items: center;
+            }
+
+            .xpRestanteText img {
+                flex-grow: 0;
+                flex-shrink: 0;
+                width: 50px;
+                height: 50px;
+                margin-left: 10px;
+            }
+
+            .xpRestanteText .description {
+                flex-grow: 1;
+                width: 120px;
+                text-align: right;
+            }
+
+            .xpRestanteText .name {
+                font-family: berlin_sans_fb_demibold;
+                font-size: 1.6em;
+                color: #ced0bb;
+                text-shadow: 0px 0px 3px rgba(0, 0, 0, 0.9);
+            }
+
+
+            `;
+            this.wGame.document.getElementsByTagName('head')[0].appendChild(jobsxpbarCss);
+
+            this.create();
+            this.updateJob();
+            this.setFightStart()
+            this.stopOnFightEnd()
+            this.stopOnFightStop()
+        }
+    }
+
+    private create(): void {
+        setTimeout(() => {
+            if (this.wGame.gui.playerData && this.wGame.gui.playerData.jobs && this.wGame.gui.playerData.jobs.list) {
+                let jobs = this.wGame.gui.playerData.jobs.list;
+                if (Object.keys(jobs).length > 0 && typeof jobs[Object.keys(jobs)[0]].experience == "undefined") this.create();
+                else {
+                    this.clean();
+                    this.xpRestanteText = document.createElement('div');
+                    this.xpRestanteText.id = 'xpRestanteId';
+                    this.xpRestanteText.className = 'xpRestanteText';
+                    this.xpRestanteText.style.visibility = 'visible';
+                    this.xpRestanteText.innerHTML = '';
+                    for (var id in jobs) {
+                        let job = jobs[id];
+                        if (job.experience.jobXpNextLevelFloor) {
+                            let xpToWin = job.experience.jobXpNextLevelFloor - job.experience.jobXP;
+                            let html = `
+                                <div class="job">
+                                    <div class="description">
+                                        <div class="name">${job.info.nameId}</div>
+                                        <div class="text">
+                                            ${xpToWin}
+                                            ${this.translate.instant('app.option.vip.jobsxp.text')}
+                                            ${job.experience.jobLevel + 1}
+                                        </div>
+                                    </div>
+                                    ${this.getIconHTML(job)}
+                                </div>
+                            `;
+
+                            this.xpRestanteText.insertAdjacentHTML('beforeend', html);
+                        }
+                    }
+                    if (this.xpRestanteText.innerHTML != '') {
+                        this.wGame.foreground.rootElement.appendChild(this.xpRestanteText);
+                    }
+                }
+            }
+            else this.create();
+        }, 500);
+    }
+
+    private setFightStart(): void {
+        this.on(this.wGame.dofus.connectionManager, 'GameFightStartingMessage', (e: any) => {
+            try {
+                this.clean();
+            } catch (ex) {
+                Logger.info(ex);
+            }
+        });
+    }
+
+    private stopOnFightEnd(): void {
+        this.on(this.wGame.dofus.connectionManager, 'GameFightEndMessage', (e: any) => {
+            try {
+                this.create();
+            } catch (ex) {
+                Logger.info(ex);
+            }
+        });
+    }
+
+    private stopOnFightStop(): void {
+        this.on(this.wGame.dofus.connectionManager, 'GameFightLeaveMessage', (e: any) => {
+            try {
+                this.create();
+            } catch (ex) {
+                Logger.info(ex);
+            }
+        });
+    }
+
+    private clean(): void {
+        if (this.xpRestanteText && this.xpRestanteText.parentElement) {
+            this.xpRestanteText.style.visibility = '';
+            this.xpRestanteText.innerHTML = '';
+            this.xpRestanteText.parentElement.removeChild(this.xpRestanteText);
+        }
+    }
+
+    private updateJob(): void {
+        this.on(this.wGame.gui, 'JobExperienceUpdateMessage', (e: any) => {
+            try {
+                if (e.experiencesUpdate.jobXpNextLevelFloor) {
+                    this.create();
+                }
+            } catch (ex) {
+                Logger.info(ex);
+            }
+        });
+    }
+
+    private getIconHTML(job): string {
+        let src = this.wGame.Config.assetsUrl + "/gfx/jobs/" + job.info.iconId + ".png";
+        return '<img src="' + src + '" />';
+    }
+
+    public reset() {
+        super.reset();
+        if (!this.params.jobsxp) {
+            let jobsxpbarCss = this.wGame.document.getElementById('jobsxpbarCss');
+            if (jobsxpbarCss && jobsxpbarCss.parentElement) {
+                jobsxpbarCss.parentElement.removeChild(jobsxpbarCss);
+            }
+            if (this.xpRestanteText && this.xpRestanteText.parentElement) {
+                this.xpRestanteText.parentElement.removeChild(this.xpRestanteText);
+            }
+        }
+    }
+
+}
--- src/app/core/mods/shortcuts/shortcuts.ts
+++ src/app/core/mods/shortcuts/shortcuts.ts
@@ -4,11 +4,13 @@ import { Mods } from "../mods";
 import { ShortcutsHelper } from "app/core/helpers/shortcuts.helper";
 import { Option } from "app/core/service/settings.service";
 import { Logger } from "app/core/electron/logger.helper";
+import { Mover } from "app/core/mods/mover/mover";
 
 export class Shortcuts extends Mods {
 
     private params: Option.Shortcuts;
     private shortcutsHelper: ShortcutsHelper;
+    private mover: Mover;
 
     constructor(wGame: any, params: Option.Shortcuts) {
         super(wGame);
@@ -18,7 +20,7 @@ export class Shortcuts extends Mods {
         if (this.params.diver.active_open_menu) {
             Logger.info(' - enable open_menu');
         }
-
+        this.mover = new Mover(this.wGame);
         this.bindAll();
     }
 
@@ -32,6 +34,42 @@ export class Shortcuts extends Mods {
             } else if (this.wGame.gui.fightManager.fightState == 1) {
               this.wGame.gui.fightManager.finishTurn();
             }
+        });
+
+        // go to top map
+        this.shortcutsHelper.bind(this.params.diver.go_up, () => {
+            this.mover.move("top", () => {
+                console.debug('Move to Up OK');
+            }, (reason: string = '') => {
+                console.debug('Move to Up Failed... (' + reason + ')');
+            });
+        });
+
+        // go to bottom map
+        this.shortcutsHelper.bind(this.params.diver.go_bottom, () => {
+            this.mover.move("bottom", () => {
+                console.debug('Move to Bottom OK');
+            }, (reason: string = '') => {
+                console.debug('Move to Bottom Failed... (' + reason + ')');
+            });
+        });
+
+        // go to left map
+        this.shortcutsHelper.bind(this.params.diver.go_left, () => {
+            this.mover.move("left", () => {
+                console.debug('Move to Left OK');
+            }, (reason: string = '') => {
+                console.debug('Move to Left Failed... (' + reason + ')');
+            });
+        });
+
+        // go to right map
+        this.shortcutsHelper.bind(this.params.diver.go_right, () => {
+            this.mover.move("right", () => {
+                console.debug('Move to Right OK');
+            }, (reason: string = '') => {
+                console.debug('Move to Right Failed... (' + reason + ')');
+            });
         });
 
         // Open chat
@@ -103,6 +141,7 @@ export class Shortcuts extends Mods {
 
     public reset() {
         super.reset();
+        if (this.mover) this.mover.reset();
         this.shortcutsHelper.unBindAll();
     }
 }
