074f1129245c2059405f941157902ca437aecd3a
Demogrogron <33393140+Demogrogron@users.noreply.github.com>
2020-05-24 00:30:14 +0200

    New pl file + some minor changes
    i guess thats all

--- electron/i18n/pl.js
+++ electron/i18n/pl.js
@@ -3,54 +3,54 @@
 module.exports = {
     "game-menu": {
         "file": {
-            "title": "Application",
-            "new-window": "New window",
-            "new-tab": "New tab",
-            "close-tab": "Close tab",
-            "close-window": "Exit"
+            "title": "Aplikacja",
+            "new-window": "Nowe okno",
+            "new-tab": "Nowa karta",
+            "close-tab": "Zamknij kartę",
+            "close-window": "Wyjście"
         },
         "edit": {
-            "title": "Edit",
-            "undo": "Undo",
-            "redo": "Redo",
-            "cut": "Cut",
-            "copy": "Copy",
-            "paste": "Paste",
-            "selectall": "Select all"
+            "title": "Edycja",
+            "undo": "Cofnij",
+            "redo": "Przywróć",
+            "cut": "Wytnij",
+            "copy": "Kopiuj",
+            "paste": "Wklej",
+            "selectall": "Zaznacz wszystko"
         },
         "window": {
-            "title": "Window",
-            "reload": "Reload",
-            "prev-tab": "Previous tab",
-            "next-tab": "Next tab",
-            "sound": "Sound",
-            "enable-sound": "Turn on sound",
-            "disable-sound": "Turn off sound",
-            "zoomin": "Zoom in",
-            "zoomout": "Zoom out",
-            "resetzoom": "Reset zoom",
-            "full-screen": "Full screen"
+            "title": "Okno",
+            "reload": "Przeładuj",
+            "prev-tab": "Poprzednia karta",
+            "next-tab": "Następna karta",
+            "sound": "Dźwięk",
+            "enable-sound": "Włącz dźwięk",
+            "disable-sound": "Wyłącz dźwięk",
+            "zoomin": "Przybliż",
+            "zoomout": "Oddal",
+            "resetzoom": "Przywróć domyślną wartość przybliżenia",
+            "full-screen": "Pełen ekran"
         },
         "infos": {
             "title": "?",
-            "changelog": "Release notes",
-            "console": "Developer console",
-            "about": "About us..."
+			"changelog": "Informacje o wydaniu",
+            "console": "Konsola deweloperska",
+            "about": "O nas..."
         }
     },
     "updater": {
         "new-update": {
-            "title": "New update %{version}",
-            "default": "A new update of Lindo is available, you can download it on our website!\n",
-            "required": "A required update of Lindo is available, you must download it on our website.",
-            "ignore": "Ignore",
-            "go-site": "Go on the website"
+            "title": "Nowa aktualizacja do wersji: %{version}",
+            "default": "Najnowsza aktualizacja Lindo jest dostępna, możesz ją pobrać na naszej stronie!\n",
+            "required": "Wymagana aktualizacja Lindo jest dostępna, pobierzesz ją z naszej stronie!",
+            "ignore": "Ignoruj",
+            "go-site": "Przejdź na stronę internetową"
         },
     },
     "options": {
         "clear-cache": {
-            "title": "Cache clared",
-            "message": "Cache has been cleared on currently opened windows"
+            "title": "Wyczyść pamięć podręczną",
+            "message": "Pamięć podręczna została wyczyszczcona na obecnie otwartych oknach"
         }
     }
 };
