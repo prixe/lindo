c44446780d87f723d6fa40476e4756d9c3a63552
Programmateur01 <git@juliencoutault.fr>
2020-10-18 19:52:35 +0200

    Ouin ouin codacy

--- src/app/core/mods/zaapSearchFilter/zaapSearchFilter.ts
+++ src/app/core/mods/zaapSearchFilter/zaapSearchFilter.ts
@@ -4,192 +4,183 @@ import { Mod } from "../mod";
 
 
 export class ZaapSearchFilter extends Mod {
-
     private styleTag: HTMLStyleElement;
     private zaapSearchContainer: HTMLDivElement;
     private zaapSearchInput : HTMLInputElement;
     private inputPlaceholder: string;
     private placeholderZaapi: string;
 
-
     startMod(): void {
         this.params = this.settings.option.vip.general.zaapsearchfilter;
-        this.inputPlaceholder = this.translate.instant('app.option.vip.zaapsearchfilter.placeholder');
-        this.placeholderZaapi = this.translate.instant('app.option.vip.zaapsearchfilter.placeholderZaapi');
+        this.inputPlaceholder = this.translate.instant("app.option.vip.zaapsearchfilter.placeholder");
+        this.placeholderZaapi = this.translate.instant("app.option.vip.zaapsearchfilter.placeholderZaapi");
 
-        
+        if (this.params) {
+            Logger.info("- enable ZaapSearchFilter");
 
-        if(this.params){
-            Logger.info('- enable ZaapSearchFilter');
-
-             this.on(this.wGame.dofus.connectionManager, 'ZaapListMessage', () => {
-                console.log("ZaapListMessage")
-               this.createSearchFilter();
-               
+            this.on(this.wGame.dofus.connectionManager, "ZaapListMessage", () => {
+                this.createSearchFilter();
             });
 
-            this.on(this.wGame.dofus.connectionManager, 'TeleportDestinationsListMessage', () => {
-                console.log("TeleportDestinationsListMessage")
+            this.on(this.wGame.dofus.connectionManager, "TeleportDestinationsListMessage", () => {
                 this.createSearchFilterZaapi();
-             });
+            });
 
-             this.on(this.wGame.connectionManager, 'LeaveDialogMessage', () => {
-                console.log("LeaveDialogMessage")
+            this.on(this.wGame.connectionManager, "LeaveDialogMessage", () => {
                 this.resetSearchFilter();
             });
         }
     }
 
 
-    private createSearchFilter(): void{
+    private createSearchFilter(): void {
         this.injectInputInDom();
 
         this.zaapSearchInput.addEventListener("change", () => {
             let zaapWanted = this.zaapSearchInput.value.toLowerCase();
 
+            let zaapList = this.wGame.document.getElementsByClassName("lindo_zaapBodyHeight__custom")[0].getElementsByClassName("row");
 
-              let zaapList = this.wGame.document.getElementsByClassName('lindo_zaapBodyHeight__custom')[0]
+            for (const currentZaap of zaapList) {
+                let destination = currentZaap.getElementsByClassName("destinationName")
 
-                .getElementsByClassName('row');
-
-           for(const currentZaap of zaapList){
-                let destination = currentZaap.getElementsByClassName('destinationName')
-
-                 if (!destination.length) continue;
+                if (!destination.length) {
+                    continue;
+                }
 
                 currentZaap.style.display = "none";
 
-                 if( currentZaap.innerText.toLowerCase().includes( zaapWanted ) ){
+                if (currentZaap.innerText.toLowerCase().includes(zaapWanted)) {
                     currentZaap.style.display = "block";
-                } 
+                }
             }
         });
     }
 
 
-    private injectInputInDom(): void{
-        this.styleTag = this.wGame.document.createElement('style');
-        this.wGame.document.getElementsByTagName('head')[0].appendChild(this.styleTag);
+    private injectInputInDom(): void {
+        this.styleTag = this.wGame.document.createElement("style");
+        this.wGame.document.getElementsByTagName("head")[0].appendChild(this.styleTag);
 
         this.styleTag.innerHTML += `
-             .lindo_zaapBodyHeight__custom{
-                 height: 70% !important;
-             }
- 
-             .lindo_zaapSearch__container{
-                 padding: 10px;
-                 width: 100%;
-             }
- 
-             .lindo_zaapSearch__input{
-                text-align: center;
-                 width: 96%;
-                 margin-right: 10px;
-                 background-color: #424242;
-                 border-radius: 5px;
-                 color: white;
-                 border-color: #262626;
-                 height: 34px;
-                 font-size: 1em;
-             }
-         `;
+        .lindo_zaapBodyHeight__custom{
+            height: 70% !important;
+        }
+
+        .lindo_zaapSearch__container{
+            padding: 10px;
+            width: 100%;
+        }
+
+        .lindo_zaapSearch__input{
+            text-align: center;
+            width: 96%;
+            margin-right: 10px;
+            background-color: #424242;
+            border-radius: 5px;
+            color: white;
+            border-color: #262626;
+            height: 34px;
+            font-size: 1em;
+        }
+        `;
 
 
-         let zaapPanels = this.wGame.document.getElementsByClassName('zaapBody')[0]
-                             .getElementsByClassName('panels')[0];
+        let zaapPanels = this.wGame.document.getElementsByClassName("zaapBody")[0]
+        .getElementsByClassName("panels")[0];
 
-         zaapPanels.classList.add('lindo_zaapBodyHeight__custom');
+        zaapPanels.classList.add("lindo_zaapBodyHeight__custom");
 
-         this.zaapSearchContainer = this.wGame.document.createElement('div');
-         this.zaapSearchInput     = this.wGame.document.createElement('input');
+        this.zaapSearchContainer = this.wGame.document.createElement("div");
+        this.zaapSearchInput     = this.wGame.document.createElement("input");
 
-         this.zaapSearchInput.setAttribute('placeholder', this.inputPlaceholder);
-         this.zaapSearchInput.setAttribute('id', 'zaapName');
+        this.zaapSearchInput.setAttribute("placeholder", this.inputPlaceholder);
+        this.zaapSearchInput.setAttribute("id", "zaapName");
 
-         this.zaapSearchContainer.classList.add('lindo_zaapSearch__container');
-         this.zaapSearchInput.classList.add('lindo_zaapSearch__input');
+        this.zaapSearchContainer.classList.add("lindo_zaapSearch__container");
+        this.zaapSearchInput.classList.add("lindo_zaapSearch__input");
 
-         this.zaapSearchContainer.append(this.zaapSearchInput);
-         this.wGame.document.getElementsByClassName('zaapBody')[0].prepend(this.zaapSearchContainer);
+        this.zaapSearchContainer.append(this.zaapSearchInput);
+        this.wGame.document.getElementsByClassName("zaapBody")[0].prepend(this.zaapSearchContainer);
     }
 
 
 
-    private createSearchFilterZaapi(): void{
+    private createSearchFilterZaapi(): void {
         this.injectInputInDomZaapi();
 
         this.zaapSearchInput.addEventListener("change", () => {
             let zaapWanted = this.zaapSearchInput.value.toLowerCase();
 
-            
-              let zaapList = this.wGame.document.getElementsByClassName('lindo_subwayBodyHeight__custom')[0]
+            let zaapList = this.wGame.document.getElementsByClassName("lindo_subwayBodyHeight__custom")[0]
+            .getElementsByClassName("row");
 
-                .getElementsByClassName('row');
+            for (const currentZaap of zaapList) {
+                let destination = currentZaap.getElementsByClassName("destinationName")
 
-           for(const currentZaap of zaapList){
-                let destination = currentZaap.getElementsByClassName('destinationName')
-
-                 if (!destination.length) continue;
+                if (!destination.length) {
+                    continue;
+                }
 
                 currentZaap.style.display = "none";
 
-                 if( currentZaap.innerText.toLowerCase().includes( zaapWanted ) ){
+                if (currentZaap.innerText.toLowerCase().includes(zaapWanted)){
                     currentZaap.style.display = "block";
-                } 
+                }
             }
         });
     }
 
 
-    private injectInputInDomZaapi(): void{
-        this.styleTag = this.wGame.document.createElement('style');
-        this.wGame.document.getElementsByTagName('head')[0].appendChild(this.styleTag);
+    private injectInputInDomZaapi(): void {
+        this.styleTag = this.wGame.document.createElement("style");
+        this.wGame.document.getElementsByTagName("head")[0].appendChild(this.styleTag);
 
         this.styleTag.innerHTML += `
-             .lindo_subwayBodyHeight__custom{
-                 height: 70% !important;
-             }
- 
-             .lindo_zaapSearch__container{
-                 padding: 10px;
-                 width: 100%;
-             }
- 
-             .lindo_zaapSearch__input{
-                text-align: center;
-                 width: 96%;
-                 margin-right: 10px;
-                 background-color: #424242;
-                 border-radius: 5px;
-                 color: white;
-                 border-color: #262626;
-                 height: 34px;
-                 font-size: 1em;
-             }
-         `;
+        .lindo_subwayBodyHeight__custom{
+            height: 70% !important;
+        }
+
+        .lindo_zaapSearch__container{
+            padding: 10px;
+            width: 100%;
+        }
+
+        .lindo_zaapSearch__input{
+            text-align: center;
+            width: 96%;
+            margin-right: 10px;
+            background-color: #424242;
+            border-radius: 5px;
+            color: white;
+            border-color: #262626;
+            height: 34px;
+            font-size: 1em;
+        }
+        `;
 
 
-         let zaapPanels = this.wGame.document.getElementsByClassName('subwayBody')[0]
-                             .getElementsByClassName('panels')[0];
+        let zaapPanels = this.wGame.document.getElementsByClassName("subwayBody")[0]
+        .getElementsByClassName("panels")[0];
 
-         zaapPanels.classList.add('lindo_subwayBodyHeight__custom');
+        zaapPanels.classList.add("lindo_subwayBodyHeight__custom");
 
-         this.zaapSearchContainer = this.wGame.document.createElement('div');
-         this.zaapSearchInput     = this.wGame.document.createElement('input');
+        this.zaapSearchContainer = this.wGame.document.createElement("div");
+        this.zaapSearchInput     = this.wGame.document.createElement("input");
 
-         this.zaapSearchInput.setAttribute('placeholder', this.placeholderZaapi);
-         this.zaapSearchInput.setAttribute('id', 'zaapName');
+        this.zaapSearchInput.setAttribute("placeholder", this.placeholderZaapi);
+        this.zaapSearchInput.setAttribute("id", "zaapName");
 
-         this.zaapSearchContainer.classList.add('lindo_zaapSearch__container');
-         this.zaapSearchInput.classList.add('lindo_zaapSearch__input');
+        this.zaapSearchContainer.classList.add("lindo_zaapSearch__container");
+        this.zaapSearchInput.classList.add("lindo_zaapSearch__input");
 
-         this.zaapSearchContainer.append(this.zaapSearchInput);
-         this.wGame.document.getElementsByClassName('subwayBody')[0].prepend(this.zaapSearchContainer);
+        this.zaapSearchContainer.append(this.zaapSearchInput);
+        this.wGame.document.getElementsByClassName("subwayBody")[0].prepend(this.zaapSearchContainer);
     }
 
 
-    private resetSearchFilter(): void{
-        if(this.styleTag){
+    private resetSearchFilter(): void {
+        if (this.styleTag) {
             this.styleTag.remove();
             this.zaapSearchInput.remove();
             this.zaapSearchContainer.remove();
