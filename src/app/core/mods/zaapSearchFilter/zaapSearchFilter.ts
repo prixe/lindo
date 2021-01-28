7eccbe199af716eb4bd419fce676679c3d907b9b
KeyTur <theketur55@gmail.com>
2021-01-28 23:55:10 +0100

    Update zaapSearchFilter.ts
    Ajout du fav / recherche sur les Prismes

--- src/app/core/mods/zaapSearchFilter/zaapSearchFilter.ts
+++ src/app/core/mods/zaapSearchFilter/zaapSearchFilter.ts
@@ -9,12 +9,15 @@ export class ZaapSearchFilter extends Mod {
     private zaapSearchInput : HTMLInputElement;
     private inputPlaceholder: string;
     private placeholderZaapi: string;
+    private placeholderPrisme: string;
+
 
     startMod(): void {
         this.params = this.settings.option.vip.general.zaapsearchfilter;
         this.inputPlaceholder = this.translate.instant("app.option.vip.zaapsearchfilter.placeholder");
         this.placeholderZaapi = this.translate.instant("app.option.vip.zaapsearchfilter.placeholderZaapi");
-
+        this.placeholderPrisme = this.translate.instant("app.option.vip.zaapsearchfilter.placeholderPrisme");
+        
         if (this.params) {
             Logger.info("- enable ZaapSearchFilter");
 
@@ -22,8 +25,16 @@ export class ZaapSearchFilter extends Mod {
                 this.createSearchFilter();
             });
 
-            this.on(this.wGame.dofus.connectionManager, "TeleportDestinationsListMessage", () => {
+            this.on(this.wGame.dofus.connectionManager, "TeleportDestinationsListMessage", (arg) => {
+                if ( arg.teleporterType === 1) {
                 this.createSearchFilterZaapi();
+            } 
+            });
+
+            this.on(this.wGame.dofus.connectionManager, "TeleportDestinationsListMessage", (arg) => {
+                if ( arg.teleporterType === 2) { // Merci TonTonJS
+                    this.createSearchFilterPrisme();
+                }      
             });
 
             this.on(this.wGame.connectionManager, "LeaveDialogMessage", () => {
@@ -36,7 +47,7 @@ export class ZaapSearchFilter extends Mod {
         }
     }
 
-
+// Zaap
     private createSearchFilter(): void {
         this.injectInputInDom();
         this.addFavInDom();
@@ -203,6 +214,177 @@ export class ZaapSearchFilter extends Mod {
         }
     }
 
+    // Prisme
+    private createSearchFilterPrisme(): void {
+        this.injectInputInDomPrisme();
+        this.addFavInDomPrism(); // Fav Prisme
+
+        this.zaapSearchInput.addEventListener("keyup", () => {
+            let zaapWanted = this.zaapSearchInput.value.toLowerCase();
+
+            let zaapList = this.wGame.document.getElementsByClassName("lindo_prismeBodyHeight__custom")[0]
+            .getElementsByClassName("row");
+
+            for (const currentZaap of zaapList) {
+                let destination = currentZaap.getElementsByClassName("destinationName");
+
+                if (!destination.length) {
+                    continue;
+                }
+
+                currentZaap.style.display = "none";
+                if (currentZaap.innerText.toLowerCase().includes(zaapWanted)) {
+                    currentZaap.style.display = "block";
+                }
+            }
+        });
+    }
+
+    private injectInputInDomPrisme(): void {
+        this.styleTag = this.wGame.document.createElement("style");
+        this.wGame.document.getElementsByTagName("head")[0].appendChild(this.styleTag);
+
+        this.styleTag.innerHTML += `
+        .lindo_prismeBodyHeight__custom{
+            height: 70% !important;
+        }
+
+        .lindo_zaapSearch__container{
+            padding: 10px;
+            width: 100%;
+        }
+
+        .lindo_zaapSearch__input{
+            text-align: center;
+            width: 96%;
+            margin-right: 10px;
+            background-color: #424242;
+            border-radius: 5px;
+            color: white;
+            border-color: #262626;
+            height: 34px;
+            font-size: 1em;
+        }
+        `;
+
+        let zaapPanels = this.wGame.document.getElementsByClassName("zaapBody")[0]
+        .getElementsByClassName("panels")[0];
+
+        zaapPanels.classList.add("lindo_prismeBodyHeight__custom");
+
+        this.zaapSearchContainer = this.wGame.document.createElement("div");
+        this.zaapSearchInput     = this.wGame.document.createElement("input");
+
+        this.zaapSearchInput.setAttribute("placeholder", this.placeholderPrisme);
+        this.zaapSearchInput.setAttribute("autofocus", "true");
+        this.zaapSearchInput.setAttribute("id", "zaapName");
+
+        this.zaapSearchContainer.classList.add("lindo_zaapSearch__container");
+        this.zaapSearchInput.classList.add("lindo_zaapSearch__input");
+
+        this.zaapSearchContainer.append(this.zaapSearchInput);
+        this.wGame.document.getElementsByClassName("zaapBody")[0].prepend(this.zaapSearchContainer);
+    }
+
+    private addFavInDomPrism() {
+        let zaapList = this.wGame.document.getElementsByClassName("lindo_prismeBodyHeight__custom")[0].getElementsByClassName("row");
+            
+        //ajout de l'étoile fovori (rempli ou non)
+        for (let index = 1; index < zaapList.length-1; index++) {
+            const currentZaap = zaapList[index];
+            
+            let divVide = currentZaap.getElementsByClassName("col")[0]
+            if(divVide.innerHTML == ''){
+                let idzaap = currentZaap.getElementsByClassName("col")[1].getElementsByClassName("destinationName")[0]
+                if(idzaap != undefined){
+                    let actionButton = (divButton, zaap) => {
+                        let favori = localStorage.getItem('zaapFav')
+                        let favoriArray = favori.split(',')
+                        let unfav = 0
+                        for (let index = 0; index < favoriArray.length; index++) {
+                            if(favoriArray[index] == zaap){
+                                unfav = index
+                            }
+                        }
+                        if(unfav != 0){
+                            favoriArray.splice(unfav,1)
+                            divButton.innerHTML = `
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 51 48">
+                                    <path fill="none" stroke="#000" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
+                                </svg>
+                                `
+                        }else{
+                            divButton.innerHTML = `
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 51 48">
+                                    <path fill="yellow" stroke="#000" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
+                                </svg>
+                                `
+                            favoriArray.push(zaap)
+                        }
+                        localStorage.setItem('zaapFav', favoriArray.toString())
+                    }
+    
+                    let divButton = document.createElement('div')
+                    divButton.onclick = () => {
+                        actionButton(divButton, idzaap.innerHTML)
+                    }
+    
+                    let favorii = localStorage.getItem('zaapFav')
+                    let favoriArrayy = favorii.split(',')
+                    let trouver = false
+                    for (let index = 0; index < favoriArrayy.length; index++) {
+                        if(favoriArrayy[index] == idzaap.innerHTML){
+                            trouver = true
+                        }
+                    }
+                    
+                    if(trouver){
+                        divButton.innerHTML = `
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 51 48">
+                            <path fill="yellow" stroke="#000" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
+                        </svg>
+                        `
+                    }else{
+                        divButton.innerHTML = `
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 51 48">
+                            <path fill="none" stroke="#000" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
+                        </svg>
+                        `
+                    }
+                    
+                    divVide.appendChild(divButton)
+                }
+            }
+        }
+        
+        //on met les favori "en haut"
+        zaapList = this.wGame.document.getElementsByClassName("lindo_prismeBodyHeight__custom")[0].getElementsByClassName("row");
+        let saveIndex = 1
+        for (let index = 1; index < zaapList.length-1; index++) {
+            let currentZaap = zaapList[index];
+            let divVide = currentZaap.getElementsByClassName("col")[0];
+            let idzaap = currentZaap.getElementsByClassName("col")[1].getElementsByClassName("destinationName")[0];
+            let favorii = localStorage.getItem('zaapFav');
+            let favoriArrayy = favorii.split(',');
+            let trouver = false
+            for (let index = 0; index < favoriArrayy.length; index++) {
+                if(idzaap != undefined)
+                    if(favoriArrayy[index] == idzaap.innerHTML){
+                        trouver = true
+                    }
+            }
+            if(trouver){
+                currentZaap.parentNode.prepend(currentZaap);
+                saveIndex++
+                index = saveIndex
+            }
+        }
+    }
+
+
+
+
+    // Zaapi
     private createSearchFilterZaapi(): void {
         this.injectInputInDomZaapi();
         this.addFavInDomZaapi(); // Fav Zaapi
@@ -274,14 +456,14 @@ export class ZaapSearchFilter extends Mod {
         this.wGame.document.getElementsByClassName("subwayBody")[0].prepend(this.zaapSearchContainer);
     }
 
-    // Zaapi Favoris
-    private addFavInDomZaapi() {
+     // Zaapi Favoris
+     private addFavInDomZaapi() {
         let zaapList = this.wGame.document.getElementsByClassName("lindo_subwayBodyHeight__custom")[0].getElementsByClassName("row");
-            
+
         //ajout de l'étoile fovori (rempli ou non)
         for (let index = 1; index < zaapList.length-1; index++) {
             const currentZaap = zaapList[index];
-            
+
             let divVide = currentZaap.getElementsByClassName("col")[0]
             if(divVide.innerHTML == ''){
                 let idzaap = currentZaap.getElementsByClassName("col")[1].getElementsByClassName("destinationName")[0]
@@ -312,12 +494,12 @@ export class ZaapSearchFilter extends Mod {
                         }
                         localStorage.setItem('zaapFav', favoriArray.toString())
                     }
-    
+
                     let divButton = document.createElement('div')
                     divButton.onclick = () => {
                         actionButton(divButton, idzaap.innerHTML)
                     }
-    
+
                     let favorii = localStorage.getItem('zaapFav')
                     let favoriArrayy = favorii.split(',')
                     let trouver = false
@@ -326,7 +508,7 @@ export class ZaapSearchFilter extends Mod {
                             trouver = true
                         }
                     }
-                    
+
                     if(trouver){
                         divButton.innerHTML = `
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 51 48">
@@ -340,12 +522,12 @@ export class ZaapSearchFilter extends Mod {
                         </svg>
                         `
                     }
-                    
+
                     divVide.appendChild(divButton)
                 }
             }
         }
-        
+
         //on met les favori "en haut"
         zaapList = this.wGame.document.getElementsByClassName("lindo_subwayBodyHeight__custom")[0].getElementsByClassName("row");
         let saveIndex = 1
@@ -369,8 +551,7 @@ export class ZaapSearchFilter extends Mod {
             }
         }
     }
-
-
+    
     private resetSearchFilter(): void {
         if (this.styleTag) {
             this.styleTag.remove();
@@ -384,3 +565,4 @@ export class ZaapSearchFilter extends Mod {
         super.reset();
     }
 }
+
