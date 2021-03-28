45cb8b61d5439a75ba70e93d0d762816b2be3e1d
Sylvain SIMON <sylvain.simon@live.fr>
2021-03-28 22:25:43 +0200

    sweetalert 8

--- src/app/core/service/prompt.service.ts
+++ src/app/core/service/prompt.service.ts
@@ -1,34 +1,42 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { default as swal, SweetAlertOptions } from 'sweetalert2';
+import { default as Swal, SweetAlertOptions } from 'sweetalert2';
 
 @Injectable()
 export class PromptService {
+
     constructor(private translate: TranslateService) {
-        swal.setDefaults({
+
+    }
+
+    close() {
+        Swal.close();
+    }
+
+    public custom(options: SweetAlertOptions) {
+
+        const Toast = Swal.mixin({
             buttonsStyling: false,
             reverseButtons: true,
             allowOutsideClick: false,
             confirmButtonClass: 'mat-primary mat-raised-button',
             cancelButtonClass: 'mat-warn mat-raised-button',
-            target: "body"
-        });
-    }
-
-    close() {
-        swal.close();
-    }
-
-    public custom(options: SweetAlertOptions) {
-        swal.setDefaults({
+            target: "body",
             type: undefined
-        });
+        })
 
-        return swal(options);
+        return Toast.fire(options);
     }
 
     public confirm(options: SweetAlertOptions) {
-        swal.setDefaults({
+
+        const Toast = Swal.mixin({
+
+            buttonsStyling: false,
+            reverseButtons: true,
+            allowOutsideClick: false,
+            target: "body",
+
             type: "question",
             title: this.translate.instant("app.prompt.title.confirm"),
 
@@ -38,48 +46,75 @@ export class PromptService {
             showCancelButton: true,
             cancelButtonText: this.translate.instant("app.prompt.button.cancel"),
             cancelButtonClass: 'mat-warn mat-raised-button',
-        });
+        })
 
-        return swal(options);
+        return Toast.fire(options);
     }
 
     public info(options: SweetAlertOptions) {
-        swal.setDefaults({
+
+        const Toast = Swal.mixin({
+
+            buttonsStyling: false,
+            reverseButtons: true,
+            allowOutsideClick: false,
+            confirmButtonClass: 'mat-primary mat-raised-button',
+            cancelButtonClass: 'mat-warn mat-raised-button',
+            target: "body",
+
             type: "info",
             title: this.translate.instant("app.prompt.title.info"),
 
             confirmButtonText: this.translate.instant("app.prompt.button.close"),
 
             showCancelButton: false
-        });
+        })
 
-        return swal(options);
+        return Toast.fire(options);
     }
 
 
     public success(options: SweetAlertOptions) {
-        swal.setDefaults({
+
+        const Toast = Swal.mixin({
+
+            buttonsStyling: false,
+            reverseButtons: true,
+            allowOutsideClick: false,
+            confirmButtonClass: 'mat-primary mat-raised-button',
+            cancelButtonClass: 'mat-warn mat-raised-button',
+            target: "body",
+
             type: "success",
             title: this.translate.instant("app.prompt.title.success"),
 
             confirmButtonText: this.translate.instant("app.prompt.button.close"),
 
             showCancelButton: false
-        });
+        })
 
-        return swal(options);
+        return Toast.fire(options);
     }
 
     public alert(options: SweetAlertOptions) {
-        swal.setDefaults({
+
+        const Toast = Swal.mixin({
+
+            buttonsStyling: false,
+            reverseButtons: true,
+            allowOutsideClick: false,
+            confirmButtonClass: 'mat-primary mat-raised-button',
+            cancelButtonClass: 'mat-warn mat-raised-button',
+            target: "body",
+
             type: "error",
             title: this.translate.instant("app.prompt.title.error"),
 
             confirmButtonText: this.translate.instant("app.prompt.button.close"),
 
             showCancelButton: false
-        });
+        })
 
-        return swal(options);
+        return Toast.fire(options);
     }
 }
