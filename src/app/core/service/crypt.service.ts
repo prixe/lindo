484f813963e34fff520ccc1025d6ef579059bb48
Sylvain SIMON <sylvain.simon@live.fr>
2021-03-28 12:39:43 +0200

    updating crypto (native)

--- src/app/core/service/crypt.service.ts
+++ src/app/core/service/crypt.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core";
-const crypto = cryptoLib;
 const algorithm = 'aes-256-ctr';
+import { createCipheriv, createDecipheriv, createHash } from 'crypto';
 
 @Injectable()
 export class CryptService {
     public encrypt(text: string, password: string) {
-        let cipher = crypto.createCipher(algorithm, password);
+        let cipher = createCipheriv(algorithm, password, null);
         let crypted = cipher.update(text, 'utf8', 'hex');
         crypted += cipher.final('hex');
 
@@ -13,7 +13,7 @@ export class CryptService {
     }
 
     public decrypt(text: string, password: string) {
-        let decipher = crypto.createDecipher(algorithm, password);
+        let decipher = createDecipheriv(algorithm, password, null);
         let dec = decipher.update(text, 'hex', 'utf8');
 
         dec += decipher.final('utf8');
@@ -22,10 +22,10 @@ export class CryptService {
     }
 
     public createHash(text: string) {
-        return crypto.createHash('sha512').update('lindo' + text).digest("hex");
+        return createHash('sha512').update('lindo' + text).digest("hex");
     }
 
     public createHashMd5(text: string) {
-        return crypto.createHash('md5').update(text).digest("hex");
+        return createHash('md5').update(text).digest("hex");
     }
 }
