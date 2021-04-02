901550b8d30104840b28733dd9f31ce45bddd24a
Sylvain SIMON <sylvain.simon@live.fr>
2021-04-02 18:31:13 +0200

    Logger helper cleanup

--- src/app/core/electron/logger.helper.ts
+++ src/app/core/electron/logger.helper.ts
@@ -1,26 +1,29 @@
-export class Logger {
+import {environment} from 'environments/environment';
 
-    public static silly(...args) {
-        LoggerLindo.silly(...args);
-    }
+export class Logger {
 
     public static verbose(...args) {
         LoggerLindo.verbose(...args);
+        if (environment.production) console.log.apply(null, args);
     }
 
     public static info(...args) {
         LoggerLindo.info(...args);
+        if (environment.production) console.info.apply(null, args);
     }
 
     public static debug(...args) {
         LoggerLindo.debug(...args);
+        if (environment.production) console.debug.apply(null, args);
     }
 
     public static warn(...args) {
         LoggerLindo.warn(...args);
+        if (environment.production) console.warn.apply(null, args);
     }
 
     public static error(...args) {
         LoggerLindo.error(...args);
+        console.error.apply(null, args);
     }
 }
