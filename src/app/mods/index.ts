7eff02c435f0ace4b2bf6de1354403a13796194a
Sylvain SIMON <sylvain.simon@live.fr>
2021-04-03 21:06:44 +0200

    Moove intersidérale : bon ça devrai faire l'affaire

---
+++ src/app/mods/index.ts
@@ -0,0 +1,16 @@
+export { General } from "../mods/general/general";
+
+export { AutoGroup } from "../mods/autoGroup/autoGroup";
+export { DamageEstimator } from "../mods/damage-estimator/damage-estimator";
+export { FightChronometer } from "../mods/fightChronometer/fightChronometer";
+export { ZaapSearchFilter } from "../mods/zaapSearchFilter/zaapSearchFilter";
+export { Harvest } from "../mods/harvestBar/harvest";
+export { HealthBar } from "../mods/healthBar/healthBar";
+export { HideMount } from "../mods/hideMount/hideMount";
+export { Jobsxp } from "../mods/jobsxp/jobsxp";
+export { Notifications } from "../mods/notifications/notifications";
+export { PartyInfo } from "../mods/partyInfo/partyInfo";
+export { PartyMember } from "../mods/partyInfo/partyMemberOnMap";
+export { RapidExchange } from "../mods/rapidExchange/rapidExchange";
+export { Shortcuts } from "../mods/shortcuts/shortcuts";
+export { ShowResources } from "../mods/showResources/showResources";
