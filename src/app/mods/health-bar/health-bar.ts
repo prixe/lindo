a5981b7fc7c9b7f08de6bf82f706bc0b20a0062c
Clover <lindo.clover@gmail.com>
2021-07-25 23:32:05 +0200

    Ensure clean health bar init & reset

--- src/app/mods/health-bar/health-bar.ts
+++ src/app/mods/health-bar/health-bar.ts
@@ -8,8 +8,7 @@ export class HealthBar extends Mod {
 
     private shortcutsHelper: ShortcutsHelper;
     private bars: { [fighterId: number]: Bar; } = { };
-    private container: HTMLDivElement;
-    private visibleBars:boolean = true;
+    private rendered: boolean = true;
 
     startMod(): void {}
 
@@ -24,9 +23,46 @@ export class HealthBar extends Mod {
         if (this.settings.option.vip.general.health_bar) this.enableHealthBars();
     }
 
+    /**
+     * Health bar mod initialization,
+     * called only when the mod has been enabled in the settings.
+     */
     private enableHealthBars(){
         Logger.info('- enable Health-Bar');
 
+        this.appendContainerStyle();
+        this.appendContainerElement();
+        this.listenGameEvents();
+
+        this.updateHealthBars();
+
+        this.shortcutsHelper.bind(this.settings.option.vip.general.health_bar_shortcut, () => this.toggleRendering());
+    }
+
+    /**
+     * The shortcut can temporarily disable bars rendering for the rest of the
+     * current fight.
+     * The container stays, but all bars are removed and stop being updated.
+     * @example
+     * this.toggleRendering() // Toggles current renderging state
+     * this.toggleRendering(true) // Forces rendering
+     */
+    private toggleRendering(rendered = !this.rendered){
+        this.rendered = rendered;
+
+        if (this.rendered) {
+            this.updateHealthBars();
+        }
+        else {
+            this.destroyHealthBars();
+        }
+    }
+
+    /**
+     * Appends style tag to the document.
+     * Sets the tag to be removed once the mod is reset.
+     */
+    private appendContainerStyle(){
         const healthbarCss = document.createElement('style');
         healthbarCss.id = 'healthbarCss';
         healthbarCss.innerHTML = `
@@ -64,70 +100,70 @@ export class HealthBar extends Mod {
 
         this.wGame.document.getElementsByTagName('head')[0].appendChild(healthbarCss);
 
-        this.standardHealthBars();
+        this.addOnResetListener(() => {
+            this.wGame.document.getElementsByTagName('head')[0].removeChild(healthbarCss);
+        });
+    }
 
-        const show = () => { this.standardHealthBars(); }
-        this.on(this.wGame.gui,'GameFightOptionStateUpdateMessage',show);
-        this.on(this.wGame.dofus.connectionManager, 'GameFightTurnStartMessage',show);
-        this.on(this.wGame.dofus.connectionManager, 'GameFightTurnEndMessage',show);
+    /**
+     * Appends the health bars container element to the document.
+     * Sets the element to be removed once the mod is reset.
+     */
+    private appendContainerElement(){
+        const container = document.createElement('div');
+        container.id = 'lifeBars';
+        container.className = 'lifeBarsContainer';
 
-        const destroy = () => { this.destroyHealthBars(); }
-        this.on(this.wGame.dofus.connectionManager, 'GameFightLeaveMessage',destroy);
-        this.on(this.wGame.dofus.connectionManager, 'GameFightEndMessage',destroy);
+        this.wGame.foreground.rootElement.appendChild(container);
 
-        this.shortcutsHelper.bind(this.settings.option.vip.general.health_bar_shortcut, () => this.toggleHealthBars());
+        this.addOnResetListener(() => {
+            this.wGame.foreground.rootElement.removeChild(container);
+        })
+    }
+
+    /**
+     * Ensures the mod updates the bars when needed
+     */
+    private listenGameEvents(){
+        // The rendering state toggled by the keyboard shortcut is reset when entering a fight
+        const ensureRendering = () => { this.toggleRendering(true); }
+        this.on(this.wGame.gui,'GameFightOptionStateUpdateMessage', ensureRendering);
+        this.on(this.wGame.dofus.connectionManager, 'GameFightTurnStartMessage', ensureRendering);
+        this.on(this.wGame.dofus.connectionManager, 'GameFightTurnEndMessage', ensureRendering);
+
+        // Update bars during a fight
+        const updateData = () => { setTimeout(() => { this.updateHealthBars(); }, 50); }
+        this.on(this.wGame.dofus.connectionManager, 'GameFightTurnStartMessage', updateData);
+        this.on(this.wGame.dofus.connectionManager, 'GameFightTurnEndMessage', updateData);
+        this.on(this.wGame.gui,'GameActionFightLifePointsGainMessage', updateData);
+        this.on(this.wGame.gui,'GameActionFightLifePointsLostMessage', updateData);
+        this.on(this.wGame.gui,'GameActionFightLifeAndShieldPointsLostMessage', updateData);
+        this.on(this.wGame.gui,'GameActionFightPointsVariationMessage', updateData);
+        this.on(this.wGame.gui,'GameFightOptionStateUpdateMessage', updateData);
+        this.on(this.wGame.gui,'GameActionFightDeathMessage', updateData);
+        this.on(this.wGame.gui,'resize', updateData);
 
         this.on(this.wGame.gui, 'GameActionFightDeathMessage', (e: any) => { this.destroyHealthBar(e.targetId); });
+
+        // Remove all bars when the fight ends
+        const destroy = () => { this.destroyHealthBars(); }
+        this.on(this.wGame.dofus.connectionManager, 'GameFightLeaveMessage', destroy);
+        this.on(this.wGame.dofus.connectionManager, 'GameFightEndMessage', destroy);
     }
 
-    private standardHealthBars(){
-        this.createHealthBars();
-        this.showHealthBars();
-    }
-
-    private toggleHealthBars(){
-        if (this.visibleBars) {this.container.style.visibility = ""; this.destroyHealthBars(); this.visibleBars = false; }
-        else { this.visibleBars = true; this.createHealthBars(); this.updateHealthBar(); }
-    }
-
-    private createHealthBars(){
-        if (this.wGame.document.getElementById('lifeBars')) return;
-        
-        this.container = document.createElement('div');
-        this.container.id = 'lifeBars';
-        this.container.className = 'lifeBarsContainer';
-
-        this.wGame.foreground.rootElement.appendChild(this.container);
-    }
-
-    private showHealthBars(){
-        if (this.container.style.visibility != "visible" && this.visibleBars){
-            this.container.style.visibility = 'visible';
-            
-            this.updateHealthBar();
-
-            const updateData = () => { setTimeout(() => { this.updateHealthBar(); }, 50); }
-            this.on(this.wGame.dofus.connectionManager, 'GameFightTurnStartMessage',updateData);
-            this.on(this.wGame.dofus.connectionManager, 'GameFightTurnEndMessage',updateData);
-            this.on(this.wGame.gui,'GameActionFightLifePointsGainMessage',updateData);
-            this.on(this.wGame.gui,'GameActionFightLifePointsLostMessage',updateData);
-            this.on(this.wGame.gui,'GameActionFightLifeAndShieldPointsLostMessage',updateData);
-            this.on(this.wGame.gui,'GameActionFightPointsVariationMessage',updateData);
-            this.on(this.wGame.gui,'GameFightOptionStateUpdateMessage',updateData);
-            this.on(this.wGame.gui,'GameActionFightDeathMessage',updateData);
-            this.on(this.wGame.gui,'resize',updateData);
-        }
-    }
-
-    private updateHealthBar() {
-        if (!this.visibleBars) return;
+    /**
+     * This will cycle through all bars and update their display if the
+     * fighter is still alive.
+     */
+    private updateHealthBars() {
+        if (!this.rendered) return;
 
         const fighters = this.wGame.gui.fightManager.getFighters();
         for (const index in fighters) {
             const fighter = this.wGame.gui.fightManager.getFighter(fighters[index]);
             if (fighter.data.alive) {
                 if (!this.bars[fighter.id]) {
-                    this.bars[fighter.id] = new Bar(fighter, this.wGame); 
+                    this.bars[fighter.id] = new Bar(fighter, this.wGame);
                     this.addOnResetListener(() => { this.destroyHealthBars(); });
                 }
                 this.bars[fighter.id].update();
@@ -135,18 +171,27 @@ export class HealthBar extends Mod {
         }
     }
 
-    private destroyHealthBar(fighterId: any){
+    /**
+     * Destroys a single health bar
+     */
+    private destroyHealthBar(fighterId: number){
         if (this.bars[fighterId]) {
             this.bars[fighterId].destroy();
             delete this.bars[fighterId];
         }
     }
 
+    /**
+     * Destroys all health bars
+     */
     private destroyHealthBars(){
-        const lifeBars = this.wGame.document.getElementById('lifeBars');
-        if (lifeBars != null) {
-            this.bars = { };
-            lifeBars.parentElement.removeChild(lifeBars);
-        }
+        Object.keys(this.bars).forEach((fighterId: string) => {
+            this.destroyHealthBar(parseInt(fighterId));
+        })
+    }
+
+    public reset(){
+        super.reset()
+        this.destroyHealthBars();
     }
 }
