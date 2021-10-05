fbe6c0863aff49028b8f8ac47b48d6ba7d469474
MirageMaPomme <ted.dark.ted@gmail.com>
2021-10-05 14:18:32 +0200

    Update grip-position-save.ts
    Changes after review

--- src/app/mods/grip-position-save/grip-position-save.ts
+++ src/app/mods/grip-position-save/grip-position-save.ts
@@ -1,8 +1,11 @@
 // TODO :
+//
 // - Use ipcRendererService instead of wGame.addEventListener('storage', ...
-// Can't make ipcRendererService works : this.ipcRendererService is well instantiate
+// Can't make ipcRendererService works : this.ipcRendererService is well instanciated
 // If you log this.ipcRendererService you see in Listener on move-grip
-// But this.ipcRendererService.send('move-grip', target) doesn't ùake anything
+// But this.ipcRendererService.send('move-grip', target) doesn't make anything
+//
+// - Calculate max top/left of grips to not overlap foreground
 
 import { SettingsService } from "@services/settings.service";
 import { TranslateService } from "@ngx-translate/core";
@@ -11,6 +14,14 @@ import { Mod } from "../mod";
 import { IpcRendererService } from "@services/electron/ipcrenderer.service";
 
 export class GripPositionSave extends Mod {
+
+  private grips = [
+    'timeline',
+    'party',
+    'notificationBar',
+    'challengeIndicator',
+    'roleplayBuffs'
+  ]
 
   constructor(
     wGame: any,
@@ -26,11 +37,11 @@ export class GripPositionSave extends Mod {
     // This is a temporary fix because ipcRendererService doesn't work
     // It works only for accounts in same Lindo window, not in multiple Lindo
     // E.G :
-    // - You have 2 windows (A, B) of 4 accounts (1, 2, 3 ...)
+    // - You have 2 windows (A, B) of 4 accounts each
     // - You move Party in Window A, Account 1
-    // - Party is moved on every accounts logged in Window A, not in Window B
+    // - Party is moved on every accounts logged in Window A, not in accounts on Window B
     wGame.addEventListener('storage', (e) => {
-      const target = e.key.substr(0, e.key.length - 8);
+      const target = e.key.slice(0, -8);
       this.moveElement(target);
     });
   }
@@ -39,9 +50,8 @@ export class GripPositionSave extends Mod {
     return;
   }
 
-  public registerEvent(skipLogin: boolean = false): void {
+  public registerEvent(skipLogin = false): void {
     const onCharacterSelectedSuccess = () => {
-
       // This doesn't work, the event is never fired
       this.on(this.ipcRendererService, 'move-grip', (event: Event, target: any) => {
         // Never displayed in console, this.moveElement never called.
@@ -49,134 +59,60 @@ export class GripPositionSave extends Mod {
         this.moveElement(target);
       });
 
-      // Timeline
-      this.on(this.wGame.gui.timeline, 'dragEnd', () => { this.timelineDragEnd() });
-
-      if (JSON.parse(localStorage.getItem("timelinePosition"))) {
-        setTimeout(() => {
-          this.moveElement('timeline');
-        }, this.getRandomTime(2, 3));
-      }
-
-      // Party
-      this.on(this.wGame.gui.party, 'dragEnd', () => {
-        this.ipcRendererService.send('move-grip', 'party');
-        this.partyDragEnd();
+      // Register events for each grips
+      this.grips.forEach(grip => {
+        this.registerGrip(grip);
       });
-
-      if (JSON.parse(localStorage.getItem("partyPosition"))) {
-        setTimeout(() => {
-          this.moveElement('party');
-        }, this.getRandomTime(2, 3));
-      }
-
-      // Observer to move party when visible, because allways pushed at right.
-      const partyObserver = new MutationObserver(() => {
-        if (this.wGame.gui.party.rootElement.style.display != 'none') {
-          this.moveElement('party');
-        }
-      });
-      partyObserver.observe(this.wGame.gui.party.rootElement, { attributes: true, childList: true });
-
-      // notificationBar
-      this.on(this.wGame.gui.notificationBar, 'dragEnd', () => { this.notificationBarDragEnd() });
-
-      if (JSON.parse(localStorage.getItem("notificationBarPosition"))) {
-        setTimeout(() => {
-          this.moveElement('notificationBar');
-        }, this.getRandomTime(2, 3));
-      }
-
-      // challengeIndicator
-      this.on(this.wGame.gui.challengeIndicator, 'dragEnd', () => { this.challengeIndicatorDragEnd() });
-
-      if (JSON.parse(localStorage.getItem("challengeIndicatorPosition"))) {
-        setTimeout(() => {
-          this.moveElement('challengeIndicator');
-        }, this.getRandomTime(2, 3));
-      }
-
-      // Observer to move challengeIndicator when visible, because allways pushed at right.
-      const challengeIndicatorObserver = new MutationObserver(() => {
-        if (this.wGame.gui.challengeIndicator.rootElement.style.display != 'none') {
-          this.moveElement('challengeIndicator');
-        }
-      });
-      challengeIndicatorObserver.observe(this.wGame.gui.challengeIndicator.rootElement, { attributes: true, childList: true });
-
-      // roleplayBuffs
-      this.on(this.wGame.gui.roleplayBuffs, 'dragEnd', () => { this.roleplayBuffsDragEnd() });
-
-      if (JSON.parse(localStorage.getItem("roleplayBuffsPosition"))) {
-        setTimeout(() => {
-          this.moveElement('roleplayBuffs');
-        }, this.getRandomTime(2, 3));
-      }
-
-      // Observer to move roleplayBuffs when visible, because allways pushed at right.
-      const roleplayBuffsObserver = new MutationObserver(() => {
-        if (this.wGame.gui.roleplayBuffs.rootElement.style.display != 'none') {
-          this.moveElement('roleplayBuffs');
-        }
-      });
-      roleplayBuffsObserver.observe(this.wGame.gui.roleplayBuffs.rootElement, { attributes: true, childList: true });
-
     };
+
+    const onResize = () => {
+      // Move grips to not overlap foreground
+      this.grips.forEach(grip => {
+        this.moveElement(grip);
+      });
+    }
 
     if (skipLogin) {
       onCharacterSelectedSuccess();
     }
 
     this.on(this.wGame.gui.playerData, "characterSelectedSuccess", onCharacterSelectedSuccess);
+    this.on(this.wGame.gui, "resize", onResize);
   }
 
   private moveElement(target) {
     if (localStorage.getItem(target + "Position")) {
       const position = JSON.parse(localStorage.getItem(target + "Position"));
 
-      this.wGame.gui[target] ?.setStyles({
-        left: position.left,
-        top: position.top
-      });
+      // Removing existing stylesheet
+      this.wGame?.document?.querySelector?.('#' + target + 'stylesheet')?.remove?.();
+
+      const stylesheet = this.wGame.document.createElement('style');
+      stylesheet.id = target + 'stylesheet';
+      // Convert first letter to uppercase to use default class
+      stylesheet.innerHTML = '.' + target.charAt(0).toUpperCase() + target.slice(1);
+      stylesheet.innerHTML += '{';
+      stylesheet.innerHTML += 'top:' + position.top + ' !important;'; // Calculate max top position to be fully and always visible
+      stylesheet.innerHTML += 'left:' + position.left + ' !important;'; // Calculate max left position to be fully and always visible
+      stylesheet.innerHTML += '}';
+      this.wGame.document.head.appendChild(stylesheet);
     }
   }
 
   private savePosition(element, top, left) {
-    const obj = {} as any;
-    obj.top = top;
-    obj.left = left;
-
+    const obj = { top, left };
     localStorage.setItem(element + "Position", JSON.stringify(obj));
   }
 
-  private timelineDragEnd() {
-    // This doesn't work : maybe event is not send, or is not listened ?
-    this.ipcRendererService.send('move-grip', 'timeline');
-    this.savePosition('timeline', this.wGame.gui.timeline.rootElement.style.top, this.wGame.gui.timeline.rootElement.style.left);
-  }
+  private registerGrip(grip: any) {
+    this.on(this.wGame.gui[grip], 'dragEnd', () => {
+      this.ipcRendererService.send('move-grip', grip);
+      this.savePosition(grip, this.wGame.gui[grip].rootElement.style.top, this.wGame.gui[grip].rootElement.style.left);
+    });
 
-  private partyDragEnd() {
-    // This doesn't work : maybe event is not send, or is not listened ?
-    this.ipcRendererService.send('move-grip', 'party');
-    this.savePosition('party', this.wGame.gui.party.rootElement.style.top, this.wGame.gui.party.rootElement.style.left);
-  }
-
-  private notificationBarDragEnd() {
-    // This doesn't work : maybe event is not send, or is not listened ?
-    this.ipcRendererService.send('move-grip', 'notificationBar');
-    this.savePosition('notificationBar', this.wGame.gui.notificationBar.rootElement.style.top, this.wGame.gui.notificationBar.rootElement.style.left);
-  }
-
-  private challengeIndicatorDragEnd() {
-    // This doesn't work : maybe event is not send, or is not listened ?
-    this.ipcRendererService.send('move-grip', 'challengeIndicator');
-    this.savePosition('challengeIndicator', this.wGame.gui.challengeIndicator.rootElement.style.top, this.wGame.gui.challengeIndicator.rootElement.style.left);
-  }
-
-  private roleplayBuffsDragEnd() {
-    // This doesn't work : maybe event is not send, or is not listened ?
-    this.ipcRendererService.send('move-grip', 'roleplayBuffs');
-    this.savePosition('roleplayBuffs', this.wGame.gui.roleplayBuffs.rootElement.style.top, this.wGame.gui.roleplayBuffs.rootElement.style.left);
+    if (JSON.parse(localStorage.getItem(grip + "Position"))) {
+      this.moveElement(grip);
+    }
   }
 
 }
