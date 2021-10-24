3ed66b3f05c9325611dd5a1d0daaa482ca02a0a7
MirageMaPomme <ted.dark.ted@gmail.com>
2021-10-24 12:39:03 +0200

    Fix moveElement
    Fix moveElement max left/top calculation

--- src/app/mods/grip-position-save/grip-position-save.ts
+++ src/app/mods/grip-position-save/grip-position-save.ts
@@ -63,8 +63,8 @@ export class GripPositionSave extends Mod {
       const cssTarget = '.' + target.charAt(0).toUpperCase() + target.slice(1);
       const targetWidth = this.wGame.document.querySelector(cssTarget).clientWidth;
       const targetHeight = this.wGame.document.querySelector(cssTarget).clientHeight;
-      const left = (position.left.slice(0, -2) <= (availableWidth - targetWidth)) ? position.left.slice(0, -2) : availableWidth - targetWidth;
-      const top = (position.top.slice(0, -2) <= (availableHeight - targetHeight)) ? position.top.slice(0, -2) : availableHeight - targetHeight;
+      const left = (position.left.slice(0, -2) < (availableWidth - targetWidth)) ? position.left.slice(0, -2) : availableWidth - targetWidth;
+      const top = (position.top.slice(0, -2) < (availableHeight - targetHeight)) ? position.top.slice(0, -2) : availableHeight - targetHeight;
 
       // Removing existing stylesheet
       this.wGame?.document?.querySelector?.('#' + target + 'stylesheet')?.remove?.();
