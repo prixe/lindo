785503570fddb3a8ca143e665e849f3b9269eb4a
Aitor Garcia Diez <bbminze@gmail.com>
2021-11-13 19:06:29 +0100

    new mod added

---
+++ src/app/mods/rune-lister/runeOpenerLister.ts
@@ -0,0 +1,86 @@
+import { TranslateService } from "@ngx-translate/core";
+import { SettingsService } from "@services/settings.service";
+import { Mod } from "../mod";
+
+export class RuneOpenerList extends Mod {
+    ID = 'RuneOpenerList';
+    langs = {
+        en: { fragmentGid: 8378, message: "Obtained (Number) (RuneName)" },
+        de: { fragmentGid: 8378, message: "Wurden erreicht (Number) (RuneName) " },
+        es: { fragmentGid: 8378, message: "Se han conseguido (Number) (RuneName) " },
+        fr: { fragmentGid: 8378, message: "Obtention de (Number) (RuneName)" },
+        it: { fragmentGid: 8378, message: "Hai ottenuto (Number) (RuneName) " },
+        pt: { fragmentGid: 8378, message: "Obteve (Number) (RuneName) " }
+    };
+    openedRune = false;
+    oldItems = []
+    messageItems = []
+    lang: { fragmentGid: number, message: string };
+    constructor(
+        wGame: any | Window,
+        settings: SettingsService,
+        translate: TranslateService,
+        language: string
+    ) {
+        super(wGame, settings, translate);
+        this.lang = this.langs[language];
+        this.load();
+    }
+
+    load() {
+        this.on(this.wGame.dofus.connectionManager, 'send', this.onSendMessage);
+        this.on(this.wGame.dofus.connectionManager, 'data', this.onReceiveMessage);
+    }
+
+    unload() {
+        this.removeListener(this.wGame.dofus.connectionManager, 'send', this.onSendMessage)
+        this.removeListener(this.wGame.dofus.connectionManager, 'data', this.onReceiveMessage)
+    }
+    onSendMessage = (msg) => {
+        console.log(msg);
+        if (!msg.data.data) return;
+        if (msg.data.data.type !== 'ObjectUseMessage') return;
+        var uid = msg.data.data.data.objectUID;
+        this.checkMagicFragment(uid)
+    }
+
+    onReceiveMessage = (msg) => {
+        if (this.openedRune) {
+            if (msg._messageType === 'ObjectQuantityMessage')
+                this.addObjectQuantity(msg.objectUID, msg.quantity);
+            else if (msg._messageType === 'InventoryWeightMessage') this.showAllMessages();
+            else if (msg._messageType === 'ObjectUseMessage') this.checkMagicFragment(msg.objectUID);
+        }
+    }
+    getGidFromUid(uid) {
+        return this.wGame.gui.playerData.inventory.objects[uid].objectGID;
+    }
+    getQuantityFromUid(uid) {
+        return this.wGame.gui.playerData.inventory.objects[uid].quantity;
+    }
+    itsARune(uid) {
+        return this.wGame.gui.playerData.inventory.objects[uid].item.type.id === 78;
+    }
+    addObjectQuantity(uid, quantity) {
+        if (!this.itsARune(uid)) return;
+        const mi = { name: this.getGidFromUid(uid), quantity: quantity - this.getQuantityFromUid(uid) };
+        this.messageItems.push(mi);
+    }
+    showAllMessages() {
+        // Display all the runes
+        this.openedRune = false;
+        var msgs = []
+        this.messageItems.forEach(mi => {
+            const msg = this.lang.message.replace("(Number)", mi.quantity).replace("(RuneName)", `$item${mi.name}`);
+            msgs.push(msg);
+        });
+        this.wGame.gui.chat.logMsg(msgs.join("\n"))
+        this.messageItems = []
+    }
+    checkMagicFragment(uid) {
+        var item = this.wGame.gui.playerData.inventory.objects[uid];
+        if (!item) return;
+        if (item.objectGID === this.lang.fragmentGid)
+            this.openedRune = true;
+    }
+}
