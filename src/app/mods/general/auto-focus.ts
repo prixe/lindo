e499f8f0fb627976b47abf1e97f4e72e4774d015
KeyTur <theketur55@gmail.com>
2021-12-22 19:52:10 +0100

    update

--- src/app/mods/general/auto-focus.ts
+++ src/app/mods/general/auto-focus.ts
@@ -4,12 +4,35 @@ export class AutoFocus extends Mod {
 
     startMod(): void {
         this.on(this.wGame.dofus.connectionManager, "ExchangeStartedBidBuyerMessage", () => {
-         this.focushdv();
+                this.focushdv();
         });
+        this.on(this.wGame.dofus.connectionManager, "FriendsListMessage", () => {
+                this.focusfriend();
+        });
+        this.wGame.gui.windowsContainer._childrenList.find(e=>e.id=="grimoire").on("open",(e)=>{
+                if (e.tabId === 'bestiary') {
+                    this.focusbestiary();
+                   }   
+        })
+        this.wGame.gui.windowsContainer._childrenList.find(e=>e.id=="grimoire").on("open",(e)=>{
+            if (e.tabId === 'achievements') {
+                this.focusarchievements();
+               }      
+    })
     }
-
-
     private focushdv(): void {
         this.wGame.document.querySelector('.BidHouseShopWindow .searchBox .InputBox input').focus()      
-    }
-}
+    };
+    private focusfriend(): void {
+        this.wGame.document.querySelector('.SocialWindow .FriendsWindow .InputBox input').focus()      
+    };
+    private focusbestiary(): void {
+        requestAnimationFrame(() => {
+            this.wGame.document.querySelector('.GrimoireWindow .BestiaryWindow .InputBox input').focus()   
+    })};
+    private focusarchievements(): void {
+        requestAnimationFrame(() => {
+            this.wGame.document.querySelector('.GrimoireWindow .AchievementsWindow .InputBox input').focus()   
+    })};
+
+ }
