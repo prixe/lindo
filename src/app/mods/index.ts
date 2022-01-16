1a547118107c768ec2374851511182d88ad502f9
KeyTur <theketur55@gmail.com>
2022-01-16 14:49:48 +0100

    Update index.ts

--- src/app/mods/index.ts
+++ src/app/mods/index.ts
@@ -2,6 +2,7 @@ export { General } from "../mods/general/general";
 
 export { AutoGroup } from "./auto-group/auto-group";
 export { DamageEstimator } from "../mods/damage-estimator/damage-estimator";
+export { EstimationPrix } from "./estimationprix/estimationprix";
 export { FightChronometer } from "./fight-chronometer/fight-chronometer";
 export { GripPositionSave } from "./grip-position-save/grip-position-save";
 export { Harvest } from "./harvest-bar/harvest";
@@ -18,4 +19,3 @@ export { ShowResources } from "./show-resources/show-resources";
 export { VerticalTimeline } from "./vertical-timeline/vertical-timeline";
 export { ZaapSearchFilter } from "./zaap-search-filter/zaap-search-filter";
 export { RuneOpenerList } from "./rune-lister/runeOpenerLister";
-export { EstimationPrix } from "./estimationprix/estimationprix"
