e9204814bce3bac09edd5f403e95daa8a927f537
KeyTur <theketur55@gmail.com>
2022-01-16 14:26:08 +0100

    Ordre alphabétique

--- src/app/mods/index.ts
+++ src/app/mods/index.ts
@@ -6,6 +6,7 @@ export { FightChronometer } from "./fight-chronometer/fight-chronometer";
 export { GripPositionSave } from "./grip-position-save/grip-position-save";
 export { Harvest } from "./harvest-bar/harvest";
 export { HealthBar } from "./health-bar/health-bar";
+export { ItemCraftable } from "./item-craftable/itemcraftable";
 export { Jobsxp } from "../mods/jobsxp/jobsxp";
 export { Marchand } from "../mods/marchand/marchand";
 export { MonsterTooltip } from "./monsterTooltip/monsterTooltip";
@@ -13,9 +14,10 @@ export { Notifications } from "../mods/notifications/notifications";
 export { PartyInfo } from "./party-info/party-info";
 export { PartyMember } from "./party-info/party-member-on-map";
 export { RapidExchange } from "./rapid-exchange/rapid-exchange";
+export { RuneOpenerList } from "./rune-lister/runeOpenerLister";
 export { Shortcuts } from "../mods/shortcuts/shortcuts";
 export { ShowResources } from "./show-resources/show-resources";
 export { VerticalTimeline } from "./vertical-timeline/vertical-timeline";
-export { ZaapSearchFilter } from "./zaap-search-filter/zaap-search-filter";
-export { RuneOpenerList } from "./rune-lister/runeOpenerLister";
-export { ItemCraftable } from "./item-craftable/itemcraftable"
+export { ZaapSearchFilter } from "./zaap-search-filter/zaap-search-filter"
+
+
