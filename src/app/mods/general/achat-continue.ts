310ff3d60b06645103ed80eec518021446f5591b
KeyTur <theketur55@gmail.com>
2022-01-16 16:53:42 +0100

    update

---
+++ src/app/mods/general/achat-continue.ts
@@ -0,0 +1,15 @@
+import {Mod} from "../mod";
+
+export class AchatContinue extends Mod {
+
+   
+    startMod(): void {
+     let boutonconfirmation =   this.wGame.gui.windowsContainer.getChildren().find(e=>e.id=="tradeItemConfirm").on("open",()=>{
+            requestAnimationFrame(() => { 
+                boutonconfirmation.buyBtn.tap()
+            })
+            
+        })
+    }
+
+ }
--- src/app/mods/general/index.ts
+++ src/app/mods/general/index.ts
@@ -1,3 +1,4 @@
+export { AchatContinue } from "./achat-continue";
 export { ChatHistory } from "./chat-history";
 export { CssOverload } from "./css-overload";
 export { HideShop } from "./hide-shop";
