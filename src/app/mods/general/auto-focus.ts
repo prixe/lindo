5b05c26cac92ed2e38bc26d461ab3692e14d2d6f
KeyTur <theketur55@gmail.com>
2022-01-16 15:24:22 +0100

    Update auto-focus.ts

--- src/app/mods/general/auto-focus.ts
+++ src/app/mods/general/auto-focus.ts
@@ -1,45 +1,33 @@
 import {Mod} from "../mod";
 
 export class AutoFocus extends Mod {
+    private focusableWindows = {
+        "ExchangeStartedBidBuyerMessage": ".BidHouseShopWindow .searchBox",
+        "ObjectItem" : ".ItemRecipesWindow .searchBox",
+        "friends" : ".SocialWindow .FriendsWindow",
+        "bestiary" : ".GrimoireWindow .BestiaryWindow",
+        "achievements" : ".GrimoireWindow .AchievementsWindow"
+    }
 
     startMod(): void { 
-        this.wGame.gui.windowsContainer._childrenList.find(e=>e.id=="itemRecipes").on("open",(e)=>{
-            setTimeout(() => {
-                this.focusRecette();
-            }, 200); 
+        this.wGame.gui.windowsContainer.getChildren().find(e=>e.id=="itemRecipes").on("open",(e)=>{
+            this.focusInput(e.itemData._type)
         })
-        this.wGame.gui.windowsContainer._childrenList.find(e=>e.id=="bidHouseShop").on("open",(e)=>{
-            this.focusHdv();
+        this.wGame.gui.windowsContainer.getChildren().find(e=>e.id=="bidHouseShop").on("open",(e)=>{
+            this.focusInput(e._messageType)
         })
-        this.wGame.gui.windowsContainer._childrenList.find(e=>e.id=="social").on("open",(e)=>{
-        this.focusFriend();
-    })
-        this.wGame.gui.windowsContainer._childrenList.find(e=>e.id=="grimoire").on("open",(e)=>{
-                if (e.tabId === 'bestiary') {
-                    this.focusBestiary();
-                   } else if (e.tabId === 'achievements') {
-                    this.focusArchievements();
-                   }      
-        })
-    }   
-    private focusRecette(): void {
-        this.wGame.document.querySelector('.ItemRecipesWindow .searchBox .InputBox input').focus()      
-    };
-    private focusHdv(): void {
-        requestAnimationFrame(() => { 
-        this.wGame.document.querySelector('.BidHouseShopWindow .searchBox .InputBox input').focus()      
-    })};
-    private focusFriend(): void { 
-        requestAnimationFrame(() => { 
-        this.wGame.document.querySelector('.SocialWindow .FriendsWindow .InputBox input').focus()      
-    })};
-    private focusBestiary(): void {
-        requestAnimationFrame(() => {
-            this.wGame.document.querySelector('.GrimoireWindow .BestiaryWindow .InputBox input').focus()   
-    })};
-    private focusArchievements(): void {
-        requestAnimationFrame(() => {
-            this.wGame.document.querySelector('.GrimoireWindow .AchievementsWindow .InputBox input').focus()   
-    })};
 
+        this.wGame.gui.windowsContainer.getChildren().find(e=>e.id=="grimoire").on("open",(e)=>{
+            this.focusInput(e.tabId)
+        })  
+      this.wGame.gui.windowsContainer.getChildren().find(e=>e.id=="social").on("open",(e)=>{
+           this.focusInput(e.tabId)
+        })
+            
+    }   
+    private focusInput(id): void {
+        requestAnimationFrame(() => { 
+        this.wGame.document.querySelector(this.focusableWindows[id] + ' .InputBox input').focus()      
+    })};
  }
+
